e42b33bbdbbd94a0bc50ea455b74507be8e1403e
Vatsal Chaturvedi <vatsalchaturvedi278@gmail.com>
2022-02-14 13:00:13 +0530

    Update model.go

--- model/model.go
+++ model/model.go
@@ -3,6 +3,8 @@ package model
 type Employee struct {
 	Name        string `json:"fullname"`
 	Employee_id string `json:"employee_id"`
+	Position    string `json:"designation"`
+	Salary      int    `json:"sal"`
 }
 
 var Employee_slice []Employee
